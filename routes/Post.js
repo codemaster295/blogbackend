8334d3be14cfa62f22a6e0e170c9fcf8da025e3f
meetmoradiya <52686520+meetmoradiya@users.noreply.github.com>
2021-12-07 10:32:59 +0530

    updated filesize limit
    updated filesize limit

--- routes/Post.js
+++ routes/Post.js
@@ -27,13 +27,13 @@ const Storage = multer.diskStorage({
 
 const upload = multer({
     storage: Storage,
-    limits: { fileSize: 10 * 1024 * 1024 },
+    limits: { fileSize: 1048576 },
     fileFilter: (req, file, cb) => {
         if (!file.originalname.match(/\.(gif|jpe?g|tiff?|png|webp|bmp)$/i)) {
             req.fileValidationError = "Only JPG OR PNG allowed!";
             return cb("Only .png and .jpg are allowed! ", false);
         }
-        else if (file.size >= 10 * 1024 * 1024) {
+        else if (file.size >= 1048576) {
             req.fileValidationError = "file size should be 10mb"
             return cb("file size should be 10mb", false);
         }
