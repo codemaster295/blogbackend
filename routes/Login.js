bdf5a085c7e4f2d48dcd223a2f80bfc0d1e4d7d8
meetmoradiya <52686520+meetmoradiya@users.noreply.github.com>
2021-12-09 12:35:08 +0530

    updated login
    updated login

--- routes/Login.js
+++ routes/Login.js
@@ -12,7 +12,7 @@ require('dotenv/config')
 router.post("/", async (req, res) => {
     try {
       const { email, password } = req.body;
-      const UserData = await loginData.findOne({ email:email }).project({ _id: 0,password:0 });
+      const UserData = await loginData.findOne({ email:email },{_id: 0})
       if (UserData && (await bcrypt.compare(password, UserData.password))) {
         // Create token
         const token = jwt.sign({ user_id: UserData._id, email }, process.env.JWT_KEY);  
